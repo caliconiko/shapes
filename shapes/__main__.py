8cb1c32d0ece856e6577b7014260243974408efb
PhotonNikko <69907559+PhotonNikko@users.noreply.github.com>
2021-11-18 20:46:30 +0700

    friendlier path stuff

--- shapes/__main__.py
+++ shapes/__main__.py
@@ -6,7 +6,7 @@ import cProfile
 
 def main():
     arg_parser = argparse.ArgumentParser(description="Shapes Interpreter for Python 3.10")
-    arg_parser.add_argument("path", type=str, help="path of file to interpret")
+    arg_parser.add_argument("path", type=str, help="path of file to interpret. if the given path doesn't have a file format, it defaults to .png")
     arg_parser.add_argument("-t", '--time', type=float, help="seconds to wait for every step")
     arg_parser.add_argument("-v", '--verbose', action='store_true',help="print extra stuff (good for debugging)")
     arg_parser.add_argument("-d", '--debug', action='store_true',help="shows what the program sees (also good for debugging)")
@@ -19,13 +19,18 @@ def main():
         cProfile.runctx("Parser(args.path, args.debug).parse_shapes()", globals(), locals(), sort="tottime")
         print("|profiled!|")
         exit()
+
+    path = args.path
+
+    if args.path[-4:] != ".png":
+        path = args.path+".png"
         
-    print("|parsing...|")
-    parser = Parser(args.path, args.debug)
+    print(f"|parsing {path}...|")
+    parser = Parser(path, args.debug)
     parse_start = time()
     shapes = parser.parse_shapes()
     parse_end = time()
-    print(f"|parsed! {parse_end-parse_start} seconds elapsed|")
+    print(f"|parsed! {round(parse_end-parse_start, 3)} seconds elapsed|")
     print("--------------------------------------")
     t=args.time
     if args.time is None:
--- shapes39/shapes/__main__.py
+++ shapes39/shapes/__main__.py
@@ -6,12 +6,12 @@ import cProfile
 
 def main():
     arg_parser = argparse.ArgumentParser(description="Shapes Interpreter for Python 3.9")
-    arg_parser.add_argument("path", type=str, help="path of file to interpret")
+    arg_parser.add_argument("path", type=str, help="path of file to interpret. if the given path doesn't have a file format, it defaults to .png")
     arg_parser.add_argument("-t", '--time', type=float, help="seconds to wait for every step")
     arg_parser.add_argument("-v", '--verbose', action='store_true',help="print extra stuff (good for debugging)")
     arg_parser.add_argument("-d", '--debug', action='store_true',help="shows what the program sees (also good for debugging)")
     arg_parser.add_argument("-p", "--profile", action='store_true', help="cprofile the parsing")
-
+    
     args = arg_parser.parse_args()
     
     if args.profile:
@@ -20,12 +20,17 @@ def main():
         print("|profiled!|")
         exit()
 
-    print("|parsing...|")
-    parser = Parser(args.path, args.debug)
+    path = args.path
+
+    if args.path[-4:] != ".png":
+        path = args.path+".png"
+        
+    print(f"|parsing {path}...|")
+    parser = Parser(path, args.debug)
     parse_start = time()
     shapes = parser.parse_shapes()
     parse_end = time()
-    print(f"|parsed! {parse_end-parse_start} seconds elapsed|")
+    print(f"|parsed! {round(parse_end-parse_start, 3)} seconds elapsed|")
     print("--------------------------------------")
     t=args.time
     if args.time is None:
